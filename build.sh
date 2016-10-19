1c2f0ccc7429e9a7f1844f7120e9bd12772958ca
fan <fanyixing111@hotmail.com>
2016-10-19 22:25:35 +0800

    add tre_eval & config.ini

--- build.sh
+++ build.sh
@@ -1,7 +1,22 @@
 
 
 #wget http://
+#wget http://
+#wget http://
+#wget http://
 
-#EIGEN_SRC="-I/home/fyx/.local/include/eigen3/ -I/home/fyx/.local/include -L/home/fyx/.local/lib"
-g++ main.cpp -o ./NN4IR NN4IR.cpp util.cpp Config.cpp -std=c++11  -O3 -w -funroll-loops -fopenmp #-g
+#tar jxf *.tar.bz2
+#tar jxf *.tar.bz2
+#tar jxf *.tar.bz2
+#tar jxf *.tar.bz2
+
+g++ main.cpp -o ./NN4IR NN4IR.cpp util.cpp Config.cpp -std=c++11  -O3 -w -funroll-loops -fopenmp 
+
+./NN4IR -config config.ini > NN4IR.log &
+
+tail -f -n 1000 NN4IR.log
+
+
+## evaluation of the ranklist
+#./trec_eval ./rob04-title/qrels.rob04.txt DRMM-LCH-IDF-rob04-title.ranklist -m all_trec
 
